cb0a5853fb578c9a1d8f933f807d985eceb03960
42xxx <mkang4@stevens.edu>
2020-09-17 15:46:10 -0400

    add tangent

--- Assignment1.py
+++ Assignment1.py
@@ -13,9 +13,13 @@ import matplotlib.pyplot as plt
 x=np.arange(0,2*np.pi,0.01)
 y_sin=np.sin(x)
 y_cos=np.cos(x)
+y_tan = np.tan(x)
 
 plt.style.use('ggplot')
 plt.plot(x,y_sin,color='r',linewidth=1,alpha=0.6,label="sin(x)")
 plt.plot(x,y_cos,color='b',linewidth=1,alpha=0.6,label='cos(x)')
+plt.plot(x,y_tan,color='y',linewidth=1,alpha=0.6,label='tan(x)')
 plt.legend(bbox_to_anchor=(0.2, 1), loc=1, borderaxespad=0.)
+plt.ylim(ymax = 2.5)
+plt.ylim(ymin = -2.5)
 plt.show()
